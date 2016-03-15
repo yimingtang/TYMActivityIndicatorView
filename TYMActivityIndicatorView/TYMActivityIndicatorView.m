6c85be5441fe24ca0850a9f699f9fe9fc76d2032
Yiming Tang <yimingnju@gmail.com>
2016-03-15 11:22:45 +0800

    Happy 2016 🎉

--- TYMActivityIndicatorView/TYMActivityIndicatorView.m
+++ TYMActivityIndicatorView/TYMActivityIndicatorView.m
@@ -3,7 +3,7 @@
 //  TYMActivityIndicatorView
 //
 //  Created by Yiming Tang on 2/9/14.
-//  Copyright (c) 2014 - 2015 Yiming Tang. All rights reserved.
+//  Copyright (c) 2014 - 2016 Yiming Tang. All rights reserved.
 //
 
 #import "TYMActivityIndicatorView.h"
